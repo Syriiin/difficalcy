eb6f5c1726e51be3d118f22e366abb1ae95319db
Samuel Cattini-Schultz <syriiins@gmail.com>
2022-06-25 19:42:27 +1000

    Add docstrings to public controller methods

--- Difficalcy/Controllers/CalculatorController.cs
+++ Difficalcy/Controllers/CalculatorController.cs
@@ -22,42 +22,63 @@ namespace Difficalcy.Controllers
             this.calculatorService = calculatorService;
         }
 
+        /// <summary>
+        /// Returns a set of information describing the calculator.
+        /// </summary>
         [HttpGet("info")]
         public ActionResult<CalculatorInfo> GetInfo()
         {
             return Ok(calculatorService.Info);
         }
 
+        /// <summary>
+        /// Returns difficulty values for a score.
+        /// </summary>
         [HttpGet("difficulty")]
         public async Task<ActionResult<TDifficulty>> GetDifficulty([FromQuery] TScore score)
         {
             return Ok(await calculatorService.GetDifficulty(score));
         }
 
+        /// <summary>
+        /// Returns performance values for a score.
+        /// </summary>
         [HttpGet("performance")]
         public async Task<ActionResult<TPerformance>> GetPerformance([FromQuery] TScore score)
         {
             return Ok(await calculatorService.GetPerformance(score));
         }
 
+        /// <summary>
+        /// Returns difficulty and performance values for a score.
+        /// </summary>
         [HttpGet("calculation")]
         public async Task<ActionResult<TCalculation>> GetCalculation([FromQuery] TScore score)
         {
             return Ok(await calculatorService.GetCalculation(score));
         }
 
+        /// <summary>
+        /// Returns difficulty values for a batch of scores.
+        /// </summary>
         [HttpPost("batch/difficulty")]
         public async Task<ActionResult<TDifficulty[]>> GetDifficultyBatch([FromBody] TScore[] scores)
         {
             return Ok(await Task.WhenAll(scores.Select(score => calculatorService.GetDifficulty(score))));
         }
 
+        /// <summary>
+        /// Returns performance values for a batch of scores.
+        /// </summary>
         [HttpPost("batch/performance")]
         public async Task<ActionResult<TPerformance[]>> GetPerformanceBatch([FromBody] TScore[] scores)
         {
             return Ok(await Task.WhenAll(scores.Select(score => calculatorService.GetPerformance(score))));
         }
 
+        /// <summary>
+        /// Returns difficulty and performance values for a batch of scores.
+        /// </summary>
         [HttpPost("batch/calculation")]
         public async Task<ActionResult<TCalculation[]>> GetCalculationBatch([FromBody] TScore[] scores)
         {
