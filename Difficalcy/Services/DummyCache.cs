6512dc4caad05f0277a4864a05973f7cc83f3436
Samuel Cattini-Schultz <syriiins@gmail.com>
2022-06-26 16:21:03 +1000

    Remove unnecessary allocation in DummyCache

--- Difficalcy/Services/DummyCache.cs
+++ Difficalcy/Services/DummyCache.cs
@@ -4,21 +4,15 @@ namespace Difficalcy.Services
 {
     public class DummyCacheDatabase : ICacheDatabase
     {
-        public async Task<string> GetAsync(string key)
-        {
-            return await Task.Run(() => (string)null);
-        }
+        public async Task<string> GetAsync(string key) => await Task.Run(() => (string)null);
 
-        public void Set(string key, string value)
-        {
-        }
+        public void Set(string key, string value) { }
     }
 
     public class DummyCache : ICache
     {
-        public ICacheDatabase GetDatabase()
-        {
-            return new DummyCacheDatabase();
-        }
+        private DummyCacheDatabase _database = new DummyCacheDatabase();
+
+        public ICacheDatabase GetDatabase() => _database;
     }
 }
