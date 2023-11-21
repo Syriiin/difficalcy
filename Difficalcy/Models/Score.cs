442eb67d0061920919bd89e5f6d76afa6a590b7d
Samuel Cattini-Schultz <syriiins@gmail.com>
2023-11-21 13:20:51 +1100

    Validate BeatmapId is required

--- Difficalcy/Models/Score.cs
+++ Difficalcy/Models/Score.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Difficalcy.Models
 {
     public abstract record Score
     {
+        [Required]
         public string BeatmapId { get; init; }
         public int? Mods { get; init; }
     }
